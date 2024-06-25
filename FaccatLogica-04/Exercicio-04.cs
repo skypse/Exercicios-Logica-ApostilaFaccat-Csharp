a95a20e1a04071a2e58526bf9614d5504c7255f1
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-25 19:51:04 -0300

    add exercicio 04

---
+++ FaccatLogica-04/Exercicio-04.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaccatLogica_04
+{
+    class Exercicio_04
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro valor: ");
+            int numero01 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor: ");
+            int numero02 = Convert.ToInt32(Console.ReadLine());
+
+            int divisao = numero01 / numero02;
+            Console.WriteLine($"A divisao dos números é: {divisao}");
+        }
+    }
+}
