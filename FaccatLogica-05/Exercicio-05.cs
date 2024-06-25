0c27021a41a995d4c89030eaca090bc7759c4e7e
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-25 19:55:23 -0300

    add exercicio 05

---
+++ FaccatLogica-05/Exercicio-05.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Escreva um algoritmo para ler um valor (do teclado) e escrever (na tela) o seu antecessor*/
+
+namespace FaccatLogica_05
+{
+    class Exercicio_05
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            string? input = Console.ReadLine();
+            int numero;
+
+            if (int.TryParse(input, out numero))
+            {
+                int antecessor = numero - 1;
+                int sucessor = numero + 1;
+
+                Console.WriteLine($"O sucessor do número {numero} é: {sucessor}");
+                Console.WriteLine($"O antecessor do número {numero} é: {antecessor}");
+            }
+            else
+            {
+                Console.WriteLine("Por favor, digite um número válido.");
+            }
+        }
+    }
+}
