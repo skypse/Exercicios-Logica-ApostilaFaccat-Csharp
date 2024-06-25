4ea8f309817a3029f3a47f234bd5f1ccaa2d4b45
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-25 20:19:45 -0300

    add exercicio 09

---
+++ FaccatLogica-09/Exercicio-09.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Escreva um algoritmo para ler o salário mensal atual de um funcionário e o 
+percentual de reajuste. Calcular e escrever o valor do novo salário. */
+
+namespace FaccatLogica_09
+{
+    class Exercicio_09
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Digite o seu salário mensal: ");
+            double salarioMensal = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o percentual de reajuste: ");
+            double percentualReajuste = Convert.ToDouble(Console.ReadLine());
+            double novoSalario = salarioMensal * (1 + percentualReajuste / 100);
+
+            Console.WriteLine($"O salário depois do reajuste é R${novoSalario:F2}");
+        }
+    }
+}
