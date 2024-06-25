d8e6599d70245e6f8fd25d13bdeac9b5eddac159
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-25 19:49:24 -0300

    add exercicio 03

---
+++ FaccatLogica-03/Exercicio-03.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaccatLogica_03
+{
+    class Exercicio_03
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro valor: ");
+            int numero01 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor: ");
+            int numero02 = Convert.ToInt32(Console.ReadLine());
+
+            int multiplicacao = numero01 * numero02;
+            Console.WriteLine($"A multiplicacao dos números é: {multiplicacao}");
+        }
+    }
+}
