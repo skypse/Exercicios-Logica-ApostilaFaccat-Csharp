1bbcde43ef6a0752db245ed6e06b5114b1a2a5b7
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-25 19:47:43 -0300

    add exercicio 02

---
+++ FaccatLogica-02/Exercicio-02.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaccatLogica_02
+{
+    class Exercicio_02
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro valor: ");
+            int numero01 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor: ");
+            int numero02 = Convert.ToInt32(Console.ReadLine());
+
+            int subtracao = numero01 - numero02;
+            Console.WriteLine($"A subtração dos números é: {subtracao}");
+        }
+    }
+}
