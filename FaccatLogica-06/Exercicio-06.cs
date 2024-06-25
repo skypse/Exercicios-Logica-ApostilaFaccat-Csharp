2c8fee5e906f63579d3077cc7380fd4783571456
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-25 20:02:54 -0300

    add exercicio 06

---
+++ FaccatLogica-06/Exercicio-06.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaccatLogica_06
+{
+    class Exercicio_06
+    {
+        public static void Main(string[]args)
+        {
+            Console.Write("Digite a base do retângulo: ");
+            double baseRetangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a altura do retângulo: ");
+            double alturaRetangulo = Convert.ToDouble(Console.ReadLine());
+
+
+            double area = baseRetangulo * alturaRetangulo;
+            Console.WriteLine($"A área do retângulo é: {area}");
+        }
+    }
+}
